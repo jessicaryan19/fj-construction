39974ccf3695ac631b196665f2875e2d018f594e
Jessica Ryan <jessicaryan0419@gmail.com>
2025-12-15 20:05:29 +0700

    feat: new about-us page

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,15 +1,11 @@
 import ServicesIntroSection from "@/components/sections/services/service-intro-section";
-import Image from "next/image";
 
 export default function Services() {
-  return (
-   <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full flex-col items-center bg-white dark:bg-black">
-        <ServicesIntroSection/>
-        <div className="rounded-3xl">
-            <Image src="/project-highlight/main-1.png" alt="Project Highlight"/>
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+            <main className="min-h-screen w-full flex flex-col gap-32 pt-40 pb-10 items-center bg-white dark:bg-black">
+                <ServicesIntroSection />
+            </main>
         </div>
-      </main>
-    </div>
-  );
+    );
 }
---
+++ src/components/sections/about-us/your-project-deserve-the-best.tsx
@@ -0,0 +1,12 @@
+import SlidingButton from "@/components/buttons/sliding-button";
+import ImageOverlayCard from "@/components/cards/image-overlay-card";
+
+export default function YourProjectDeserveTheBest() {
+    return (
+        <ImageOverlayCard image="/about/image-2.jpg">
+            <h1 className="text-white text-7xl uppercase">Your Project Deserves The Best</h1>
+            <h1 className="text-white">See how we can support your next build.</h1>
+            <SlidingButton text="Explore Our Services" href="/services" />
+        </ImageOverlayCard>
+    )
+}
